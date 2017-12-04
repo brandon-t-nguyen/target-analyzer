a858cd1cc99f3cfa9fa0958d785851656eb26f08
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2017-12-03 21:18:01 -0600

    Turned the analyzer into its own class

--- target_analyzer.py
+++ target_analyzer.py
@@ -100,6 +100,74 @@ def draw_circles(circles, cimg, cross_size):
         print('Mean: (%d, %d)' %(meanX, meanY))
     cv2.imshow('output',cimg)
 
+class Analyzer:
+    def __init__(self):
+        self.R = -1
+        self.N = -1
+        self.S = -1
+        self.C = -1
+        self.roi_p1 = [-1, -1]
+        self.roi_p2 = [-1, -1]
+
+        self.scale_p1 = [-1, -1]
+        self.scale_p2 = [-1, -1]
+
+        # images
+        self.img   = None
+        self.sel   = None
+        self.pproc = None
+        self.edges = None
+        self.result= None
+        self.output= None
+
+        self.circles= None
+
+    def set_image(self, img):
+        self.img = img
+
+    def set_roi(self, roi_p1, roi_p2):
+        self.roi_p1 = roi_p1[:]
+        self.roi_p2 = roi_p2[:]
+
+    def set_scale(self, norm_d, diameter, real_dis, scale_p1, scale_p2):
+        self.N = norm_d
+        self.R = diameter / 2;
+
+        self.scale_p1 = scale_p1[:];
+        self.scale_p2 = scale_p2[:];
+        line_dis = float(math.sqrt(math.pow(self.scale_p2[0] - self.scale_p1[0], 2) +
+                                   math.pow(self.scale_p2[1] - self.scale_p1[1], 2)))
+        self.C = line_dis / real_dis;
+        self.S = self.N / (self.R * self.C)
+
+    def run(self):
+        self.sel = get_selection(self.img, self.roi_p1, self.roi_p2, self.S)
+
+        # 5: Perform preprocessing
+        self.pproc = preprocess(self.sel)
+
+        # 6: Do the Hough
+        hough = Hough()
+        hough.dp        = 1.25
+        hough.canny     = 70
+        hough.minDist   = int(round(self.N * 1.00))
+        hough.minRadius = int(round(self.N * 0.75))
+        hough.maxRadius = int(round(self.N * 1.25))
+        #hough.accum     = int(round(2 * math.pi * self.N * 0.75))
+        #hough.accum     = int(round(2 * math.pi * self.N * 0.25))
+        hough.accum     = int(round(self.N))
+        print("Hough accumulator threshold: %d" % hough.accum)
+
+        self.edges   = hough.runCanny(self.pproc)
+        self.circles = hough.runHough(self.pproc)
+
+        self.result = cv2.cvtColor(self.sel, cv2.COLOR_GRAY2BGR)
+        draw_circles(self.circles, self.result, 20)
+
+        transform_circles(self.circles, self.S, self.roi_p1)
+        self.output = cv2.cvtColor(self.img, cv2.COLOR_GRAY2BGR)
+        draw_circles(self.circles, self.output, 40)
+
 def main():
     if len(sys.argv) > 1:
         image_path = sys.argv[1]
@@ -112,9 +180,10 @@ def main():
 
     # Create window with freedom of dimensions
     cv2.namedWindow("original"  , cv2.WINDOW_NORMAL)
-    cv2.namedWindow("roi", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("roi"       , cv2.WINDOW_NORMAL)
     cv2.namedWindow("preprocess", cv2.WINDOW_NORMAL)
     cv2.namedWindow("edges"     , cv2.WINDOW_NORMAL)
+    cv2.namedWindow("result"    , cv2.WINDOW_NORMAL)
     cv2.namedWindow("output"    , cv2.WINDOW_NORMAL)
 
     img = cv2.imread(image_path, 0)
@@ -123,9 +192,11 @@ def main():
 
     cv2.setMouseCallback("original", scale_callback)
 
+    analyzer = Analyzer()
+    analyzer.set_image(img)
+
     # 1: Get the bullet diameter from user
     D = float(input("Input bullet diameter (in):: "))
-    R = D/2
 
     # 2: Get the pixel distance:real distance factor
     global p0, p1
@@ -138,12 +209,9 @@ def main():
     # 3: Get the scaling factor for an ROI
     # N: target size
     # C: conversion factor: px/in
-
     real_dis = float(input("Input actual distance (in):: "))
-    C =  line_dis / real_dis
-    N = 20
-    S = N / (R * C)
-    print("Scaling factor: %f" % S)
+    analyzer.set_scale(20, D, real_dis, p0, p1)
+    print("Scaling factor: %f" % analyzer.S)
 
     # 4: Get the ROI
     p0 = [-1, -1]
@@ -152,50 +220,24 @@ def main():
         print("Select an ROI, then hit any key")
         cv2.waitKey(0)
     print("Selected (%d, %d) to (%d, %d)" %(p0[0], p0[1], p1[0], p1[1]))
+    analyzer.set_roi(p0, p1)
 
-    sel = get_selection(img, p0, p1, S)
-    off = p0
-    cv2.imshow("roi", sel)
+    # 5: Run
+    analyzer.run()
 
-    # 5: Perform preprocessing
-    pproc = preprocess(sel)
-    cv2.imshow("preprocess", pproc)
-
-    # 6: Do the Hough
-    hough = Hough()
-    hough.dp        = 1.25
-    hough.canny     = 70
-    hough.minDist   = int(round(N * 1.00))
-    hough.minRadius = int(round(N * 0.75))
-    hough.maxRadius = int(round(N * 1.25))
-    #hough.accum     = int(round(2 * math.pi * N * 0.75))
-    #hough.accum     = int(round(2 * math.pi * N * 0.25))
-    hough.accum     = int(round(N))
-    print("Hough accumulator threshold: %d" % hough.accum)
-
-    edges   = hough.runCanny(pproc)
-    cv2.imshow("edges", edges)
-
-    circles = hough.runHough(pproc)
-
-    # 7: Draw the results
-
-    # saving one that uses only the ROI
-    cimg_roi = cv2.cvtColor(sel, cv2.COLOR_GRAY2BGR)
-    draw_circles(circles, cimg_roi, 20)
-
-    transform_circles(circles, S, off)
-    cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    draw_circles(circles, cimg, 40)
-    cv2.imshow("output", cimg)
+    cv2.imshow("roi"        , analyzer.sel)
+    cv2.imshow("preprocess" , analyzer.pproc)
+    cv2.imshow("edges"      , analyzer.edges)
+    cv2.imshow("result"     , analyzer.result)
+    cv2.imshow("output"     , analyzer.output)
 
     cv2.waitKey(0)
 
     # 8: Save results
-    cv2.imwrite("out_1%s.png" % result_name, sel);
-    cv2.imwrite("out_2%s.png" % result_name, pproc);
-    cv2.imwrite("out_3%s.png" % result_name, edges);
-    cv2.imwrite("out_4%s.png" % result_name, cimg_roi);
+    cv2.imwrite("out_1%s.png" % result_name, analyzer.sel);
+    cv2.imwrite("out_2%s.png" % result_name, analyzer.pproc);
+    cv2.imwrite("out_3%s.png" % result_name, analyzer.edges);
+    cv2.imwrite("out_4%s.png" % result_name, analyzer.result);
 
     cv2.destroyAllWindows()
 
